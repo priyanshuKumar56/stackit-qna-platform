0a0c3c2dc07517e8944ecf05a89bda29f79356ae
Priyanshujawra <priyanshukumar93861@gmail.com>
2025-07-12 13:00:18 +0530

    some change

--- components/question-detail.tsx
+++ components/question-detail.tsx
@@ -112,7 +112,6 @@ export function QuestionDetail({ question }: QuestionDetailProps) {
         </CardContent>
       </Card>
 
-          <NestedComments questionId={question.id} />
       <Card>
         <CardContent className="p-6">
           <h3 className="text-lg font-semibold mb-4">Reply</h3>
@@ -129,6 +128,7 @@ export function QuestionDetail({ question }: QuestionDetailProps) {
         </CardContent>
       </Card>
 
+          <NestedComments questionId={question.id} />
     </div>
   )
 }
